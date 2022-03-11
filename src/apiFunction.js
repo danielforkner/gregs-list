168c9493a753a6c627e563b4d5997af504c335d4
Jaden <jadenshort231@gmail.com>
2022-03-11 13:38:39 -0600

    added the posts with descriptions

--- src/apiFunction.js
+++ src/apiFunction.js
@@ -1,12 +1,14 @@
 const BASEURL = "https://strangers-things.herokuapp.com"
 const COHORT = "/api/2202-FTB-ET-WEB-FT"
 
-export const getAllPosts = async () => {
+export const getAllPosts = async (setAllPosts) => {
  try {
      const response = await fetch(`${BASEURL}${COHORT}/posts`)
      const data = await response.json()
-     console.log(data)
- } catch (error) {
+   setAllPosts(data.data.posts)
+     if(response.error) throw response.error 
+   
+} catch (error) {
      console.error(error)
  }
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { getAllPosts } from "../apiFunction";
 
 const Posts = (props) => {
-    getAllPosts()
-  return (
-    <div> <h1>Hello this is the post container</h1> 
-    </div>
-  );
+    const [allPosts, setAllPosts] = useState([])
+    useEffect(() => {
+        getAllPosts(setAllPosts)
+
+        console.log(allPosts)
+    }, [])
+
+
+
+    return (
+        <div> <h1>Hello this is the post container</h1>
+        {allPosts.map((post,i)=>{
+            return(
+          <div key={i} className="post">  <h4>{post.title} </h4>
+            <p>{post.description}</p> </div> 
+            )
+        })}
+        </div>
+    );
 };
 
 export default Posts;
