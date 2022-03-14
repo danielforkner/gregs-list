f7e25a5ba558236337c8b73f563c67f9bbcbffa1
danielforkner <daniel.forkner@gmail.com>
2022-03-14 15:05:11 -0400

    createPost function works. also started to add routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Posts, Register, Navbar, Login, Newpost } from './components';
 import './index.css';
 
 ReactDom.render(
   <Router>
-    <Navbar />
-    <Posts />
-    {/* <Register /> */}
-    <Login />
-    <Newpost />
+    <Switch>
+      <Route path="/posts/newpost">
+        <Newpost />
+      </Route>
+      <Route path="/posts">
+        <Navbar />
+        <Posts />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/">
+        <Login />
+      </Route>
+    </Switch>
   </Router>,
   document.getElementById('app')
 );
