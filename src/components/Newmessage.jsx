9a77de346c6030d76eb9b8efa7d0909a4cfa84f9
Jaden <jadenshort231@gmail.com>
2022-03-15 10:30:57 -0500

    worked on the submit button

--- src/components/Newmessage.jsx
+++ src/components/Newmessage.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Newmessage = () => {
+  const[message,setMessage] = useState("") 
   return (
     <div>
-      <textarea></textarea>
-      <button>Submit Reply</button>
+      <form onSubmit={(e) => {e.preventDefault()
+      setMessage('')}}>
+      <textarea className="replyText" value={message} onChange ={(e) =>{
+        setMessage(e.target.value)
+      }}></textarea>
+      <button type="submit"> submit Reply</button>
       <button>Cancel Reply</button>
+      </form>
     </div>
   );
 };
