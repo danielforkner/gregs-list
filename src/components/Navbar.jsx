215a7534d4b4598ee6dcb25e427bc23bcfd298d9
Jaden <jadenshort231@gmail.com>
2022-03-16 14:13:44 -0500

    added logo and style to nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useEffect } from 'react/cjs/react.production.min';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react/cjs/react.production.min";
+import Gregs_list from "../Gregs_list.png";
 const NavBar = ({ isLoggedIn, setIsLoggedIn }) => {
   // useEffect(() => {
   //   if (window.localStorage.getItem('token')) {
@@ -11,39 +11,36 @@ const NavBar = ({ isLoggedIn, setIsLoggedIn }) => {
 
   return (
     <div className="nav_bar_container">
+      <img src={Gregs_list} />
       <div className="link_container">
-        <div>
-          <div className="nav_item">
-            {isLoggedIn ? (
-              <button
-                onClick={() => {
-                  window.localStorage.removeItem('token');
-                  setIsLoggedIn(false);
-                }}
-              >
-                Logout
-              </button>
-            ) : (
-              <>
-                <Link to="/register">
-                  <button>Register</button>
-                </Link>
-                <Link to="/login">
-                  <button>Login</button>
-                </Link>
-              </>
-            )}
-
-            <Link to="/posts">
-              <button>All Posts</button>
+        {isLoggedIn ? (
+          <button
+            onClick={() => {
+              window.localStorage.removeItem("token");
+              setIsLoggedIn(false);
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <>
+            <Link to="/register">
+              <button>Register</button>
             </Link>
-            {isLoggedIn ? (
-              <Link to="/profile">
-                <button>My Profile</button>
-              </Link>
-            ) : null}
-          </div>
-        </div>
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+          </>
+        )}
+
+        <Link to="/posts">
+          <button>All Posts</button>
+        </Link>
+        {isLoggedIn ? (
+          <Link to="/profile">
+            <button>My Profile</button>
+          </Link>
+        ) : null}
       </div>
     </div>
   );
